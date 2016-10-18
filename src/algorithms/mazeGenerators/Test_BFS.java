6b6dc66636d877d214a2230096eef507f9d717d1
Administrator <Administrator@WIN-4VEE9DHIE8C.localdomain>
2016-10-18 07:32:14 -0700

    test Start Position Is Null

--- src/algorithms/mazeGenerators/Test_BFS.java
+++ src/algorithms/mazeGenerators/Test_BFS.java
@@ -51,6 +51,26 @@ public class Test_BFS {
 		assertTrue(bfs.getNumberOfNodesEvaluated() == 0);
 		
 	}
+	
+	/**
+	 * Test method for {@link algorithms.search.BFS#search(algorithms.search.Searchable)}.
+	 * tests that code doesn't throw exception if StartPosition is null
+	 */
+	@Test
+	public void testStartPositionIsNull() {
+
+		Maze3dGenerator generator = new GrowingTreeGenerator();
+		Maze3d maze = generator.generate(10, 10, 10);
+		assertNotNull(maze);
+		maze.setStartPosition(null);
+		
+		MazeAdapter adapter = new MazeAdapter(maze);
+		BFS<Position> bfs = new BFS<Position>();
+		Solution<Position> solution = bfs.search(adapter);
+		assertNull(solution);
+		assertTrue(bfs.getNumberOfNodesEvaluated() == 0);
+		
+	}
 
 
 }
